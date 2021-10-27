36111bd3442383f99b1ca0c0b8f60dc1b0157c4e
Enrique Sampaio <enrique.sampaio@brainboss.com.br>
2021-10-27 16:38:17 -0300

    Updating readme

--- src/main/scala/br/com/brainboss/lzodf/lzodf.scala
+++ src/main/scala/br/com/brainboss/lzodf/lzodf.scala
@@ -4,27 +4,24 @@ import org.apache.spark.sql.SparkSession
 
 object lzodf extends App {
   val usage = """
-    Usage: lzodf <source table name> <output-path> <parquet table name>
+    Usage: lzodf <parquet/snappy file destination> <original table name>
   """
 
-  if (args.length < 3)
+  if (args.length < 2)
     println(usage)
   else {
     val spark = SparkSession.builder().appName("lzodf").getOrCreate()
 
-    val srcTableName = args(0)
-    val outputPath = args(1)
-    val tableName = args(2)
+    val outputPath = args(0)
+    val tableName = args(1)
 
-    val df = spark.sql(s"SELECT * FROM ${srcTableName}")
-
-    df.show(10)
+    val df = spark.sql(s"SELECT * FROM ${tableName}")
 
     df
       .write.mode(org.apache.spark.sql.SaveMode.Overwrite)
       .option("compression", "snappy")
       .option("path", outputPath)
       .format("parquet")
-      .saveAsTable(tableName)
+      .saveAsTable(s"${tableName}_snappy")
   }
 }
