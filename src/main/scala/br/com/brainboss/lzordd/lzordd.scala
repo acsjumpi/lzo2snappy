f9deccbd4d65dd76d37273123ed89e7adba0f237
Enrique Sampaio <enrique.sampaio@brainboss.com.br>
2021-10-27 15:37:27 -0300

    Improving arguments, adding output path to create external table on dataframe method

--- src/main/scala/br/com/brainboss/lzordd/lzordd.scala
+++ src/main/scala/br/com/brainboss/lzordd/lzordd.scala
@@ -3,28 +3,32 @@ package br.com.brainboss.lzordd
 import org.apache.spark.sql.SparkSession
 
 object lzordd extends App {
-      val spark = SparkSession.builder().appName("lzordd").getOrCreate()
-      val sc = spark.sparkContext
+  val usage = """
+    Usage: lzordd <input-path> <output-path> <table name> [delimiter]
+  """
 
-      val usage = """
-            Usage: lzordd <input-path> <output-path> <table name> [delimiter]
-      """
+  if (args.length < 3)
+    println(usage)
+  else {
+    val spark = SparkSession.builder().appName("lzordd").getOrCreate()
+    val sc = spark.sparkContext
 
-      if (args.length < 3) println(usage)
-      val inputPath = args(0)
-      val outputPath = args(1)
-      val tableName = args(2)
-      val delimiter = if (args.length == 4) args(3) else null
+    val inputPath = args(0)
+    val outputPath = args(1)
+    val tableName = args(2)
+    val delimiter = if (args.length == 4) args(3) else ","
 
-      // TODO: Need to catch the correct exceptions
-      try {
+    // TODO: Need to catch the correct exceptions
+    try {
 
-            val read = readLzo(sc, inputPath, delimiter)
-            val tableSchema = createAndWriteSnappy(spark, tableName, read, outputPath)
+      val read = readLzo(sc, inputPath, delimiter)
+      val tableSchema = createAndWriteSnappy(spark, tableName, read, outputPath)
 
-            readAndCreateTable(spark, tableSchema, tableName, outputPath)
+      readAndCreateTable(spark, tableSchema, tableName, outputPath)
 
-      } catch { case e:Exception=>e.printStackTrace() }
-
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
 }
 
