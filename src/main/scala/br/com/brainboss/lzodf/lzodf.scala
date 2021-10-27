981a89ea7c1736df807f7b99f94b1e05e70e4ffd
Enrique Sampaio <enrique.sampaio@brainboss.com.br>
2021-10-27 15:37:27 -0300

    Improving arguments, adding output path to create external table on dataframe method

--- src/main/scala/br/com/brainboss/lzodf/lzodf.scala
+++ src/main/scala/br/com/brainboss/lzodf/lzodf.scala
@@ -3,20 +3,32 @@ package br.com.brainboss.lzodf
 import org.apache.spark.sql.SparkSession
 
 object lzodf extends App {
-  val spark = SparkSession.builder().appName("lzodf").getOrCreate()
+  val usage = """
+    Usage: lzodf <input-path> <output-path> <table name> [delimiter]
+  """
 
-  val inputPath = args(0)
-  val outputPath = args(1)
+  if (args.length < 3)
+    println(usage)
+  else {
+    val spark = SparkSession.builder().appName("lzodf").getOrCreate()
 
-  //spark.conf.set("spark.sql.parquet.compression.codec","codec")
-  val df = spark.read.csv(inputPath)
+    val inputPath = args(0)
+    val outputPath = args(1)
+    val tableName = args(2)
+    val delimiter = if (args.length == 4) args(3) else ","
 
-  df.show(10)
+    val df = spark
+      .read
+      .option("delimiter", delimiter)
+      .csv(inputPath)
 
-  df
-    .write.mode(org.apache.spark.sql.SaveMode.Overwrite)
-    .option("compression", "snappy")
-    .format("parquet")
-    .saveAsTable(outputPath)
+    df.show(10)
 
+    df
+      .write.mode(org.apache.spark.sql.SaveMode.Overwrite)
+      .option("compression", "snappy")
+      .option("path", outputPath)
+      .format("parquet")
+      .saveAsTable(tableName)
+  }
 }
